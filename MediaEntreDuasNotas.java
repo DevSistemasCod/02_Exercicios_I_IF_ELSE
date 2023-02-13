77c01fe7c419be5c07b98925e5a2e626867c2217
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-02-12 23:08:18 -0300

    Update MediaEntreDuasNotas.java

--- MediaEntreDuasNotas.java
+++ MediaEntreDuasNotas.java
@@ -5,22 +5,20 @@ public class MediaEntreDuasNotas {
 
 	public static void main(String[] args) {
 		float nota1, nota2, media;
-		Scanner entrada1 = new Scanner(System.in);
-		Scanner entrada2 = new Scanner(System.in);
+		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Digite a nota 1: ");
-		nota1 = entrada1.nextFloat();
+		nota1 = entrada.nextFloat();
 		
 		System.out.println("Digite a nota 2: ");
-		nota2 = entrada2.nextFloat();
+		nota2 = entrada.nextFloat();
 		
 		media = (nota1+nota2)/2;
 		
 		System.out.print("média é:");
 		System.out.print(media);
 		
-		entrada1.close();
-		entrada2.close();
+		entrada.close();
 	}
 
 }
